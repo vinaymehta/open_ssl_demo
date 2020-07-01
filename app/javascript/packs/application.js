d95368766be72918f05b82ffcd5c99917fe85448
vinaymehta <vinay.ymca@gmail.com>
2020-07-01 14:24:07 +0530

    added code for generating RSA, CSR and encoded RSA

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,10 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require('jquery')
+
+require("packs/index")
+
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
